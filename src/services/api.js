405722b3f8ed99fff611ff0997949068aa88a04f
joaopsilv <joao_p_silveira@estudante.sc.senai.br>
2025-04-02 14:02:48 -0300

    refactor: Axios para comunicação com a API

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,12 @@
+import axios from "axios"
+
 const API_VERSION = "15.3.1"
 const BASE_URL = `https://ddragon.leagueoflegends.com/cdn/${API_VERSION}/data/en_US`
 
 export const fetchChampions = async () => {
 	try {
-		const response = await fetch(`${BASE_URL}/champion.json`)
-		const data = await response.json()
-		return Object.values(data.data)
+		const response = await axios.get(`${BASE_URL}/champion.json`)
+		return Object.values(response.data.data)
 	} catch (error) {
 		console.error("Error searching for champions:", error)
 		return []
@@ -14,9 +15,8 @@ export const fetchChampions = async () => {
 
 export const fetchChampionById = async (championId) => {
 	try {
-		const response = await fetch(`${BASE_URL}/champion/${championId}.json`)
-		const data = await response.json()
-		return data.data[championId]
+		const response = await axios.get(`${BASE_URL}/champion/${championId}.json`)
+		return response.data.data[championId]
 	} catch (error) {
 		console.error(`Error fetching champion details ${championId}:`, error)
 		return null
