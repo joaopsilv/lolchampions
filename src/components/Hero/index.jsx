4c1fa46e6fa3c18b5359f714d7e3ce454798daff
joaopsilv <joao_p_silveira@estudante.sc.senai.br>
2025-04-03 11:23:33 -0300

    fix: Problema no Hero com o Vercel

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,14 +1,11 @@
+import bgImg from "../../assets/images/bg-home.jpg"
 import Scroll from "../Scroll"
 import style from "./Hero.module.scss"
 
 export default function Hero() {
 	return (
 		<section className={`flex-column center ${style.hero}`}>
-			<img
-				src="src/assets/images/bg-home.jpg"
-				alt="League of Legends Champions"
-				className="bg-img"
-			/>
+			<img src={bgImg} alt="League of Legends Champions" className="bg-img" />
 			<h1>
 				<span>Champions</span>
 				<span>Summary</span>
